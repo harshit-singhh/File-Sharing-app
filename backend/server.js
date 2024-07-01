0527ad761feaeee601ea3e0eedb836b9bac81ec4
Harshit Singh <harshitsingh81203@gmail.com>
2024-07-01 23:02:59 +0530

    changed some code in server.js and int index.js

--- backend/server.js
+++ backend/server.js
@@ -27,7 +27,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname1, '/frontend/build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname1, "frontend/build", "index.html"));
+                  //resolve
+    res.sendFile(path.join(__dirname1, "frontend/build/index.html"));
   })
 }
 else {
