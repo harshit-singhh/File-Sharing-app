fac094a0b4bc9655c84128fe1cff8383d48a29b5
Harshit Singh <harshitsingh81203@gmail.com>
2024-07-01 17:31:44 +0530

    added code for deployment in package.json of backend folder

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const router = require('./routes/routes');
 const cors = require("cors");
 const DBconnection = require('./database/db')
 const colors = require('colors');
+const path = require('path');
 
 
 const app = express();
@@ -15,6 +16,27 @@ app.use(cors());
 app.use('/', router);
 
 const PORT = process.env.PORT;
+
+
+
+// ---------------------------DEPLOYMENT----------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname1, '/frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  })
+}
+else {
+  app.get("/", (req, res) => {
+    res.send("API is Running successfully");
+  })
+}
+
+
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`.yellow.underline);
